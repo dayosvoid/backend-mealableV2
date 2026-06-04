c8946938fd32b948d07b4103903e76463bcbfdd3
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-04 05:28:18 +0100

    wip(security): setup security against cyber attacks and implementation of config env variables

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require("express")
-const app = express()
-const dotenv = require("dotenv")
-dotenv.config()
-const PORT = process.env.PORT || 5000
-const mongoose = require("mongoose")
-const customError = require("./utilis/CustomError")
-const errorHandler = require("./middleware/errorHandling.middleware")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const customError = require("./utilis/CustomError");
+const errorHandler = require("./middleware/errorHandling.middleware");
+const { MONGO_URI, PORT } = require("./config/config");
+const { default: helmet } = require("helmet");
+const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
+app.use(helmet()); // Protects against XSS, clickjacking, and script injection
+app.use(
+  cors({
+    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedOrigins: ["*"], // Allow all origins (for development; restrict in production)
+    allowedMethods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+); // Enable CORS for all routes
 
+app.use("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
-
-app.use(errorHandler)
+app.use(errorHandler);
 
 startServer = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
-        })
-    } catch (err) {
-        console.error("Error starting server:", err.message);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error starting server:", err.message);
+    process.exit(1);
+  }
+};
 
-startServer()
+startServer();
