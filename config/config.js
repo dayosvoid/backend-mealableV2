0cbfaf546cb97bf35487d1fc3be765cf9dbfc135
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-05 04:17:25 +0100

    fix: improper use of the JWT_SECRET

--- config/config.js
+++ config/config.js
@@ -4,6 +4,14 @@ config({
   path: `.env.${process.env.NODE_ENV === "production" ? "production" : "development"}.local`,
 });
 
-const { NODE_ENV, BASE_URI, MONGO_URI, PORT, RESEND_KEY } = process.env;
+const { NODE_ENV, BASE_URI, MONGO_URI, PORT, RESEND_KEY, JWT_SECRET } =
+  process.env;
 
-module.exports = { NODE_ENV, BASE_URI, MONGO_URI, PORT, RESEND_KEY };
+module.exports = {
+  NODE_ENV,
+  BASE_URI,
+  MONGO_URI,
+  PORT,
+  RESEND_KEY,
+  JWT_SECRET,
+};
