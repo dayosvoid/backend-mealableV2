567f5f2a800231fe8ad4dd23a7cb61fd8b3d2f80
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-09 23:57:09 +0100

    chore: add connection log for mongoose database setup

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ app.use(errorHandler);
 const startServer = async () => {
   try {
     await mongoose.connect(MONGO_URI);
+
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected");
+    });
+
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
