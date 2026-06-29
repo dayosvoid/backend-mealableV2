65b9eea0db01473092ad3bfe2b6e831257d507dd
Adedayo <atimileyin911@gmail.com>
2026-06-29 10:53:55 +0100

    file

--- config/config.js
+++ config/config.js
@@ -1,11 +1,27 @@
 const { config } = require("dotenv");
 
-config({
-  path: `.env.${process.env.NODE_ENV === "production" ? "production" : "development"}.local`,
-});
+// Only load .env file in development — on Render variables are already in process.env
+if (process.env.NODE_ENV !== "production") {
+  config(); // loads .env locally
+}
 
-const { NODE_ENV, BASE_URI, MONGO_URI, PORT, RESEND_KEY, JWT_SECRET, JWT_REFRESH_SECRET, SESSION_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, FRONTEND_URL, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME } =
-  process.env;
+const {
+  NODE_ENV,
+  BASE_URI,
+  MONGO_URI,
+  PORT,
+  RESEND_KEY,
+  JWT_SECRET,
+  JWT_REFRESH_SECRET,
+  SESSION_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  FRONTEND_URL,
+  REDIS_HOST,
+  REDIS_PASSWORD,
+  REDIS_PORT,
+  REDIS_USERNAME,
+} = process.env;
 
 module.exports = {
   NODE_ENV,
@@ -22,5 +38,5 @@ module.exports = {
   REDIS_HOST,
   REDIS_PASSWORD,
   REDIS_PORT,
-  REDIS_USERNAME
+  REDIS_USERNAME,
 };
